7e748671355e574437eb06d474f79f1bcad70628
SAKSHAM SINGH <sakshamsingh4848.com>
2024-08-12 23:18:04 +0530

    Front end done

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             Order your favorite food here:
         </h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes and bumba bumba rumba rumba bammba bamba khumba khumba</p>
-   <button>View Menu</button>
+   <button onClick={<a></a>} >View Menu</button>
     </div>
     </div>
   )
